6f8d2eb0307eb9745017df75c281a6e0853bbe3f
dawid <dawid.widulinski@gmail.com>
2023-07-17 12:38:24 +0200

    init commit

---
+++ src/main/java/com/widulinski/trellocloner/TrelloClonerApplication.java
@@ -0,0 +1,13 @@
+package com.widulinski.trellocloner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloClonerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloClonerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/widulinski/trellocloner/TrelloClonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widulinski.trellocloner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloClonerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
